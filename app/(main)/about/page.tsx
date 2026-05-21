0132cb370fa52dfe6d787841b9fa794fecd65c89
fyrmuen <fyrmuenn@gmail.com>
2026-05-21 13:51:04 +0700

    test

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -2,7 +2,17 @@ export default function AboutPage() {
   return (
     <main className="container-shell py-8 animate-fadeUp">
       <h1 className="mb-3 font-serif text-4xl">About Grove</h1>
-      <p className="max-w-[760px] text-[13px] leading-[1.8] text-grove-muted2">Grove adalah investment research platform yang fokus pada keputusan, bukan noise. Kami membangun framework yang konsisten agar investor punya struktur berpikir lintas kondisi pasar.</p>
+      <p className="max-w-[760px] text-[13px] leading-[1.8] text-grove-muted2">
+        Grove adalah investment research platform yang fokus pada keputusan,
+        bukan noise. Kami membangun framework yang konsisten agar investor punya
+        struktur berpikir lintas kondisi pasar.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
+        sapiente similique corrupti assumenda eos, cupiditate soluta sint
+        explicabo aliquam aspernatur, laudantium libero perspiciatis maxime
+        corporis aut voluptatem magnam! Ea, placeat!
+      </p>
     </main>
   );
 }
